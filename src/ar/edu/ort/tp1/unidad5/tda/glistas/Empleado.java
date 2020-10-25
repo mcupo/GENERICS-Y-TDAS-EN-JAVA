ff5ce5b40e8015968dc22e3655afcd2d40642318
Marco Cupo <cuttlas88@yahoo.com.ar>
2020-10-25 02:25:10 -0300

    Ejemplos listas

---
+++ src/ar/edu/ort/tp1/unidad5/tda/glistas/Empleado.java
@@ -0,0 +1,34 @@
+package ar.edu.ort.tp1.unidad5.tda.glistas;
+
+public class Empleado {
+
+	private int legajo;
+	private String nombre;
+
+	public Empleado(int legajo, String nombre) {
+		this.setLegajo(legajo);
+		this.setNombre(nombre);
+	}
+
+	public int getLegajo() {
+		return legajo;
+	}
+
+	private void setLegajo(int legajo) {
+		this.legajo = legajo;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	@Override
+	public String toString() {
+		return "Empleado [legajo=" + legajo + ", nombre=" + nombre + "]";
+	}
+
+}
